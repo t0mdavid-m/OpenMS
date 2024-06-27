eebd5dd52a1b29ef580cfe44bc024ba6338b7681
Tom David Müller <tom.mueller@beachouse.de>
2024-06-27 14:12:09 +0200

    Revert "system libraries 2"
    This reverts commit 05e2dc931752285d0f8854b1a5af8ed18d86eedf.

--- tools/ci/cibuild.cmake
+++ tools/ci/cibuild.cmake
@@ -39,11 +39,8 @@ ${VAR_NAME}=$ENV{${VAR_NAME}}"
   endif()
 endmacro()
 
-#### Install System runtime libraries into /bin, so NSIS picks them up; this saves us from shipping a VC-Redist.exe with the installer
-set(CMAKE_INSTALL_OPENMP_LIBRARIES TRUE)
-set (CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION ${INSTALL_LIB_DIR})
+# include the InstallRequiredSystemLibraries to ensure system dependencies are prepared
 include(InstallRequiredSystemLibraries)
-
 
 # same but for multiple variables
 macro(add_env_vars_to_cache_if_exists VAR_NAMES)
