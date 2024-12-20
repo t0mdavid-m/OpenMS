5c7bf622ea42a16d3c45d09e5e614ba094cb8809
KyowonJeong <jeong@informatik.uni-tuebingen.de>
2024-12-20 21:20:47 +0100

    SpectralDeconvolution_test debugging

--- src/openms/source/ANALYSIS/TOPDOWN/SpectralDeconvolution.cpp
+++ src/openms/source/ANALYSIS/TOPDOWN/SpectralDeconvolution.cpp
@@ -1241,6 +1241,7 @@ namespace OpenMS
     std::vector<PeakGroup> filtered_pg_vec;
     filtered_pg_vec.reserve(dspec.size());
     std::vector<float> overlap_intensity(dspec.size(), .0f);
+    dspec.sort();
 
     for (Size i = 0; i < dspec.size(); i++)
     {
@@ -1306,7 +1307,7 @@ namespace OpenMS
   void SpectralDeconvolution::removeOverlappingPeakGroups_(DeconvolvedSpectrum& dspec, double tol, PeakGroup::TargetDecoyType target_decoy_type)
   {
     if (dspec.empty()) { return; }
-
+    dspec.sort();
     std::vector<PeakGroup> filtered_pg_vec; //
     filtered_pg_vec.reserve(dspec.size());
 
@@ -1364,6 +1365,7 @@ namespace OpenMS
     if (excluded_masses.empty()) return;
     std::vector<PeakGroup> filtered_pg_vec; //
     filtered_pg_vec.reserve(dspec.size());
+    dspec.sort();
 
     for (const auto& peak_group : dspec)
     {
