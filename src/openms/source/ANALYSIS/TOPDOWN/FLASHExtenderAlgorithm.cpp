437db08db592341c30073b20c44516e0b0f8a4b3
Tom David Mueller <tom.mueller@beachouse.de>
2026-01-08 11:06:02 +0100

    enable flashextender debug

--- src/openms/source/ANALYSIS/TOPDOWN/FLASHExtenderAlgorithm.cpp
+++ src/openms/source/ANALYSIS/TOPDOWN/FLASHExtenderAlgorithm.cpp
@@ -14,7 +14,7 @@
 namespace OpenMS
   {
   inline const int multi_ion_score = 1;
-  inline const bool debug = false;
+  inline const bool debug = true;
   inline const double i2f_mass = Residue::getInternalToFull().getMonoWeight();
   FLASHExtenderAlgorithm::FLASHExtenderAlgorithm(): DefaultParamHandler("FLASHExtenderAlgorithm"), ProgressLogger()
   {
